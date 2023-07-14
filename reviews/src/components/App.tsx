00114d0f1840daa216d51f68520c90030f51234e
Aleksandr Kadulin <aleksandr.kadulin@insly.com>
2023-07-14 16:44:05 +0300

    add reviews

---
+++ reviews/src/components/App.tsx
@@ -0,0 +1,15 @@
+import { Review } from ".";
+
+export const App = () => {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+};
