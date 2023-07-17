2a3f88e0ef76c5b0908d939a2d99eb8b62aed03e
Aleksandr Kadulin <aleksandr.kadulin@insly.com>
2023-07-17 16:13:42 +0300

    initial commit

---
+++ menu/src/components/Menu.tsx
@@ -0,0 +1,30 @@
+interface IMenu {
+  id: number;
+  title: string;
+  category: string;
+  price: number;
+  img: string;
+  desc: string;
+}
+
+export const Menu = ({ items }: { items: IMenu[] }) => {
+  return (
+    <div className="section-center">
+      {items.map((menuItem) => {
+        const { id, title, img, desc, price } = menuItem;
+        return (
+          <article key={id} className="menu-item">
+            <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">${price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
---
+++ menu/src/components/index.ts
@@ -0,0 +1,3 @@
+export * from "./App";
+export * from "./Categories";
+export * from "./Menu";
